e271b9687a8e98edd4ffa2b842d03547a3f1f619
ccyyn <cynthia4@andrew.cmu.edu>
2022-02-05 15:56:12 -0500

    hi

--- main.py
+++ main.py
@@ -1,2 +1,56 @@
-
+from inspect import formatargvalues
+from sys import displayhook
 import tkinter
+import threading
+from tkinter import *
+
+pcount = 0
+
+# def make(): 
+root = Tk()
+root.attributes("-fullscreen", True)
+
+title = Label(root, text = "p n u m b e r")
+text = Message(root, text = pcount, font = 20000)
+xButton = Button(root, text="x", command=root.destroy)
+
+title.pack()
+text.pack()
+xButton.pack()
+# return root
+
+
+def display():
+    # title.pack()
+    text.configure(text = pcount, font = 20000)
+    # xButton.pack()
+
+def pupdate():
+    global pcount 
+    pcount += 1
+    print(pcount)
+
+
+lookingAway = True
+while(True):
+    if(lookingAway): 
+        display()
+        root.update()
+        # root.deiconify() #may need this for toplevel
+        # root.attributes('-topmost', True) #also not working for toplevel :()
+        root.after(5000, root.withdraw())
+
+        pupdate()
+
+        # display()
+        #root.destroy()
+        # root = make()
+
+        # pupdate()
+        # root.after(4000, root.destroy())
+
+# b = True
+# while(b):
+#     print(1)
+#     b = False
+    # root.mainloop()
