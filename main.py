0d430d4534eb91a30c9b70b1279bd245c335ac53
almutwakel <almutwakelhassan@gmail.com>
2022-02-05 15:42:47 -0500

    main loop

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import tkinter
 import math
 from model import pose
-
+import time
 
 mark_detector = pose.MarkDetector()
 cap = pose.cv2.VideoCapture(0)
@@ -27,71 +27,80 @@ camera_matrix = pose.np.array(
      [0, focal_length, center[1]],
      [0, 0, 1]], dtype="double"
 )
-while True:
+# while True:
+
+def detect():
     ret, img = cap.read()
-    if ret == True:
-        faceboxes = mark_detector.extract_cnn_facebox(img)
-        for facebox in faceboxes:
-            face_img = img[facebox[1]: facebox[3],
-                       facebox[0]: facebox[2]]
-            face_img = pose.cv2.resize(face_img, (128, 128))
-            face_img = pose.cv2.cvtColor(face_img, pose.cv2.COLOR_BGR2RGB)
-            marks = mark_detector.detect_marks([face_img])
-            marks *= (facebox[2] - facebox[0])
-            marks[:, 0] += facebox[0]
-            marks[:, 1] += facebox[1]
-            shape = marks.astype(pose.np.uint)
-            # mark_detector.draw_marks(img, marks, color=(0, 255, 0))
-            image_points = pose.np.array([
-                shape[30],  # Nose tip
-                shape[8],  # Chin
-                shape[36],  # Left eye left corner
-                shape[45],  # Right eye right corne
-                shape[48],  # Left Mouth corner
-                shape[54]  # Right mouth corner
-            ], dtype="double")
-            dist_coeffs = pose.np.zeros((4, 1))  # Assuming no lens distortion
-            (success, rotation_vector, translation_vector) = pose.cv2.solvePnP(model_points, image_points, camera_matrix,
-                                                                          dist_coeffs, flags=pose.cv2.SOLVEPNP_UPNP)
+    faceboxes = mark_detector.extract_cnn_facebox(img)
+    for facebox in faceboxes:
+        face_img = img[facebox[1]: facebox[3],
+                   facebox[0]: facebox[2]]
+        face_img = pose.cv2.resize(face_img, (128, 128))
+        face_img = pose.cv2.cvtColor(face_img, pose.cv2.COLOR_BGR2RGB)
+        marks = mark_detector.detect_marks([face_img])
+        marks *= (facebox[2] - facebox[0])
+        marks[:, 0] += facebox[0]
+        marks[:, 1] += facebox[1]
+        shape = marks.astype(pose.np.uint)
+        # mark_detector.draw_marks(img, marks, color=(0, 255, 0))
+        image_points = pose.np.array([
+            shape[30],  # Nose tip
+            shape[8],  # Chin
+            shape[36],  # Left eye left corner
+            shape[45],  # Right eye right corne
+            shape[48],  # Left Mouth corner
+            shape[54]  # Right mouth corner
+        ], dtype="double")
+        dist_coeffs = pose.np.zeros((4, 1))  # Assuming no lens distortion
+        (success, rotation_vector, translation_vector) = pose.cv2.solvePnP(model_points, image_points, camera_matrix,
+                                                                      dist_coeffs, flags=pose.cv2.SOLVEPNP_UPNP)
 
-            # Project a 3D point (0, 0, 1000.0) onto the image plane.
-            # We use this to draw a line sticking out of the nose
+        # Project a 3D point (0, 0, 1000.0) onto the image plane.
+        # We use this to draw a line sticking out of the nose
 
-            (nose_end_point2D, jacobian) = pose.cv2.projectPoints(pose.np.array([(0.0, 0.0, 1000.0)]), rotation_vector,
-                                                             translation_vector, camera_matrix, dist_coeffs)
+        (nose_end_point2D, jacobian) = pose.cv2.projectPoints(pose.np.array([(0.0, 0.0, 1000.0)]), rotation_vector,
+                                                         translation_vector, camera_matrix, dist_coeffs)
 
-            for p in image_points:
-                pose.cv2.circle(img, (int(p[0]), int(p[1])), 3, (0, 0, 255), -1)
+        for p in image_points:
+            pose.cv2.circle(img, (int(p[0]), int(p[1])), 3, (0, 0, 255), -1)
 
-            p1 = (int(image_points[0][0]), int(image_points[0][1]))
-            p2 = (int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
-            x1, x2 = pose.draw_annotation_box(img, rotation_vector, translation_vector, camera_matrix)
+        p1 = (int(image_points[0][0]), int(image_points[0][1]))
+        p2 = (int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
+        x1, x2 = pose.draw_annotation_box(img, rotation_vector, translation_vector, camera_matrix)
 
-            pose.cv2.line(img, p1, p2, (0, 255, 255), 2)
-            pose.cv2.line(img, tuple(x1), tuple(x2), (255, 255, 0), 2)
-            # for (x, y) in shape:
-            #     pose.cv2.circle(img, (x, y), 4, (255, 255, 0), -1)
-            # pose.cv2.putText(img, str(p1), p1, font, 1, (0, 255, 255), 1)
-            try:
-                m = (p2[1] - p1[1]) / (p2[0] - p1[0])
-                ang1 = int(math.degrees(math.atan(m)))
-            except:
-                ang1 = 90
+        pose.cv2.line(img, p1, p2, (0, 255, 255), 2)
+        pose.cv2.line(img, tuple(x1), tuple(x2), (255, 255, 0), 2)
+        # for (x, y) in shape:
+        #     pose.cv2.circle(img, (x, y), 4, (255, 255, 0), -1)
+        # pose.cv2.putText(img, str(p1), p1, font, 1, (0, 255, 255), 1)
+        try:
+            m = (p2[1] - p1[1]) / (p2[0] - p1[0])
+            ang1 = int(math.degrees(math.atan(m)))
+        except:
+            ang1 = 90
 
-            try:
-                m = (x2[1] - x1[1]) / (x2[0] - x1[0])
-                ang2 = int(math.degrees(math.atan(-1 / m)))
-            except:
-                ang2 = 90
+        try:
+            m = (x2[1] - x1[1]) / (x2[0] - x1[0])
+            ang2 = int(math.degrees(math.atan(-1 / m)))
+        except:
+            ang2 = 90
 
-                # print('div by zero error')
-            pose.cv2.putText(img, str(ang1), tuple(p1), font, 2, (128, 255, 255), 3)
-            pose.cv2.putText(img, str(ang2), tuple(x1), font, 2, (255, 255, 128), 3)
-        pose.cv2.imshow('img', img)
-        if pose.cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    else:
-        break
-pose.cv2.destroyAllWindows()
-cap.release()
+            # print('div by zero error')
+        pose.cv2.putText(img, str(ang1), tuple(p1), font, 2, (128, 255, 255), 3)
+        pose.cv2.putText(img, str(ang2), tuple(x1), font, 2, (255, 255, 128), 3)
+    pose.cv2.imshow('img', img)
+    if pose.cv2.waitKey(1) & 0xFF == ord('q'):
+        return
+    return
 
+
+if __name__ == "__main__":
+    for i in range(100):
+        angle = detect()
+        if angle < 30 or angle < -30:
+            print("looked away")
+
+    pose.cv2.destroyAllWindows()
+    cap.release()
+
+tkinter.mainloop()
